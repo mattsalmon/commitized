f1238ce3017d0b0052c36fa282f784fb5a43ed8d
Matt Salmon <matt.salmon@tote.co.uk>
2021-09-07 23:19:24 +0100

    feat: very basic, hard-coded initial version running

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using LibGit2Sharp;
+using System.Linq;
+
+namespace commitized
+{
+    class Program
+    {
+        static List<string> validTypes = new() {"build", "ci", "docs", "feat", "fix", "perf", "refactor", "style", "test", "chore"};
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"dotnet cz v{versionString}");
+                Console.WriteLine("-------------");
+                Console.WriteLine("\nUsage:");
+                Console.WriteLine("  dotnet cz <message>");
+                return;
+            }
+
+            CheckCommitMessagesForType(string.Join(' ', args));
+        }
+
+        static void CheckCommitMessagesForType(string message)
+        {
+            using (var repo = new Repository(Directory.GetCurrentDirectory()))
+            {
+                var invalidCommits = repo.Commits.Where(c => validTypes.TrueForAll(t=>!c.Message.StartsWith(t)));
+                foreach (var commit in invalidCommits)
+                {
+                    Console.WriteLine(
+                        $"Commit id {commit.Id.ToString().Substring(0, 7)} " +
+                        $"does not start with a valid type : " +
+                        $"{commit.MessageShort}");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # commitized
 Like commitizen, but for dotnet
+Has a dependency on the library LibGit2Sharp
+
+## Setup
+Commitized is installed using Nuget
+> dotnet add package Commitized
+
+In the first version, Commitized is hard-coded to look for the following types in commit messages:
+- "build",
+- "ci",
+- "docs",
+- "feat",
+- "fix",
+- "perf",
+- "refactor",
+- "style",
+- "test",
+- "chore"
